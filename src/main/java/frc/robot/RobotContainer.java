a1ece8303bbb1b7dd24d60437f242b56a337d471
Jason <jasonwedwards09@gmail.com>
2025-11-10 20:18:05 -0600

    Adds basic Swerve functionality with starting pose2d position

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,62 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj2.command.button.CommandGenericHID;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.drive.DriveSubsystem;
+
+import java.io.File;
+import java.util.function.DoubleSupplier;
+
+import swervelib.SwerveInputStream;
 
 public class RobotContainer {
+
+    private final CommandXboxController driverController;
+
+    private DriveSubsystem drive;
+
     public RobotContainer() {
+
+        driverController = new CommandXboxController(0);
+
+        drive = new DriveSubsystem();
+
         configureBindings();
     }
+
+    public void teleopInit() {
+        registerSwerveAngularVelocityDrive(() -> getJoystickInput(driverController, 0), () -> getJoystickInput(driverController, 1), () -> getJoystickInput(driverController, 4));
+    }
+
+    public void registerSwerveAngularVelocityDrive(DoubleSupplier x, DoubleSupplier y, DoubleSupplier rotation) {
+        SwerveInputStream driveAngularVelocity = SwerveInputStream.of(
+                        drive.getSwerveDrive(), x, y) // Axis which give the desired translational angle and speed.
+                .withControllerRotationAxis(rotation) // Axis which give the desired angular velocity.
+                .deadband(Constants.Controller.DEADZONE_CONSTANT) // Controller deadband
+                .scaleTranslation(Constants.Controller.SCALE_TRANSLATION) // Scaled controller translation axis
+                .allianceRelativeControl(
+                        false); // Alliance relative controls. Done already in the driver configuration files.
+        Command driveFieldOrientedAnglularVelocity = drive.driveFieldOriented(driveAngularVelocity);
+        drive.setDefaultCommand(driveFieldOrientedAnglularVelocity);
+    }
+
+    private double getJoystickInput(CommandXboxController stick, int axe) {
+        return stick.getRawAxis(axe);
+    }
+
+    
 
     private void configureBindings() {}
 
---
+++ src/main/java/frc/robot/drive/DriveSubsystem.java
@@ -0,0 +1,84 @@
+package frc.robot.drive;
+
+import static edu.wpi.first.units.Units.Meter;
+import static edu.wpi.first.units.Units.Feet;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import swervelib.parser.SwerveParser;
+import swervelib.SwerveDrive;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+
+public class DriveSubsystem extends SubsystemBase{
+    
+    double maximumSpeed = 1.0;
+
+    SwerveDrive swerve;
+    File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "swerve");
+
+    public DriveSubsystem() {
+
+        boolean blueAlliance = false;
+        Pose2d startingPose = blueAlliance ? new Pose2d(new Translation2d(Meter.of(1),
+                                                                      Meter.of(4)),
+                                                    Rotation2d.fromDegrees(0))
+                                       : new Pose2d(new Translation2d(Meter.of(16),
+                                                                      Meter.of(4)),
+                                                    Rotation2d.fromDegrees(180));
+        SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+
+        try {
+        swerve = new SwerveParser(swerveJsonDirectory).createSwerveDrive(maximumSpeed, startingPose);
+        } catch (IOException e){
+            System.out.println("Swerve drive configuration file could not be found at "
+                        + Filesystem.getDeployDirectory()
+                        + "/swerve");
+                e.printStackTrace();
+        }
+    }
+
+    public SwerveDrive getSwerveDrive() {
+        return swerve;
+    }
+
+    public Command driveFieldOriented(Supplier<ChassisSpeeds> velocity) {
+        return run(() -> {
+            ChassisSpeeds logVelocity = velocity.get();
+            SmartDashboard.putNumber("swerveX", logVelocity.vxMetersPerSecond);
+            SmartDashboard.putNumber("swerveY", logVelocity.vyMetersPerSecond);
+            SmartDashboard.putNumber("swerveR", logVelocity.omegaRadiansPerSecond);
+
+            swerve.driveFieldOriented(logVelocity);
+        });
+    }
+
+    /**
+   * Command to drive the robot using translative values and heading as angular velocity.
+   *
+   * @param translationX     Translation in the X direction.
+   * @param translationY     Translation in the Y direction.
+   * @param angularRotationX Rotation of the robot to set
+   * @return Drive command.
+   */
+    public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier angularRotationX) {
+        return run(() -> {
+            // Make the robot move
+            swerve.drive(new Translation2d(translationX.getAsDouble() * swerve.getMaximumChassisVelocity(),
+                                                translationY.getAsDouble() * swerve.getMaximumChassisVelocity()),
+                                angularRotationX.getAsDouble() * swerve.getMaximumChassisAngularVelocity(),
+                                true,
+                                false);
+            });
+}}
